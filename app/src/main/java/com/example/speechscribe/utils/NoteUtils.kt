e5e6083d0421ef702d204b7b7ba7ea0abd040ba8
Adarsh Mohan <adarshmohankalleril@outlook.com>
2020-07-19 02:34:15 +0530

    Update NoteUtils.kt

--- app/src/main/java/com/example/speechscribe/utils/NoteUtils.kt
+++ app/src/main/java/com/example/speechscribe/utils/NoteUtils.kt
@@ -4,9 +4,7 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-/**
- * Created by ixi.Dv on 13/05/2018.
- */
+
 object NoteUtils {
     /**
      * @param time that will be convert  and formatted to string
